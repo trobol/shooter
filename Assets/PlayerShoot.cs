0978790e61a47afbc035fd0cbbacdc2b17c2eb95
thorfernbacher <thorfernbacher@gmail.com>
2019-02-03 17:05:57 -0500

    adding gun and improved shooting

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+	bool triggerDown;
+	float triggerTime = 0;
+	public float chargeTime = 2f;
+	public int bulletCount = 5;
+	public float maxSpread = 15f;
+	public GameObject indicatorL, indicatorR;
+	GameObject bullet;
+	public GameObject gun;
+	float reloadTime = 0;
+	void Start()
+	{
+		bullet = Resources.Load<GameObject>("Bullet");
+	}
+	void Update()
+	{
+		if (triggerTime < chargeTime)
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				indicatorL.SetActive(true);
+				indicatorR.SetActive(true);
+			}
+			if (Input.GetMouseButtonUp(0))
+			{
+				Shoot();
+				triggerTime = 0;
+			}
+			if (Input.GetMouseButton(0))
+			{
+				triggerTime += Time.deltaTime;
+			}
+		}
+		else
+		{
+			Shoot();
+		}
+		RenderIndicators();
+	}
+	void Shoot()
+	{
+
+		indicatorL.SetActive(false);
+		indicatorR.SetActive(false);
+		for (int i = 0; i < bulletCount; i++)
+		{
+			Quaternion rot = gun.transform.rotation * Quaternion.Euler(0, ((((float)i / (float)bulletCount) * 2f) - 1f) * (maxSpread - (maxSpread * (triggerTime / chargeTime))), 0);
+			rot.x = 0;
+			rot.z = 0;
+			GameObject g = Instantiate(bullet, gun.transform.position + (gun.transform.forward), rot);
+			g.GetComponent<Rigidbody>().velocity = g.transform.forward * 20;
+			Destroy(g, 1);
+		}
+		triggerTime = 0;
+	}
+	void RenderIndicators()
+	{
+
+		float angle = maxSpread - (maxSpread * (triggerTime / chargeTime));
+		indicatorL.transform.localEulerAngles = new Vector3(90, 0, angle);
+		indicatorR.transform.localEulerAngles = new Vector3(90, 0, -angle);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	Move move = new Move();
+	Rigidbody rb;
+	public Camera cam;
+	Vector3 camOffset;
+	public float camSpeed = 1;
+	public float speed = 1;
+	public float rotSpeed = 1;
+	Animator animator;
+	public GameObject o;
+	float stunTime;
+	public bool grounded = false;
+	public GameObject gun;
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		camOffset = cam.transform.position - transform.position;
+		rb = GetComponent<Rigidbody>();
+
+	}
+	void Update()
+	{
+		MoveUpdate();
+		if (Input.GetMouseButtonDown(1))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+
+
+	}
+	void MoveUpdate()
+	{
+		move.Update();
+		Vector3 pos = transform.position;
+		pos.y = 0;
+		Plane plane = new Plane(Vector3.up, pos);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float distance;
+		if (plane.Raycast(ray, out distance))
+		{
+			Vector3 hitPoint = ray.GetPoint(distance);
+			o.transform.position = hitPoint;
+			hitPoint.y = transform.position.y;
+			Quaternion rot = Quaternion.LookRotation(hitPoint - transform.position);
+			Quaternion diff = (transform.rotation * Quaternion.Inverse(rot));
+			if (diff.eulerAngles.y < 35 || diff.eulerAngles.y > 325)
+			{
+				hitPoint.y = gun.transform.position.y;
+				Quaternion gunRot = Quaternion.LookRotation(hitPoint - gun.transform.position) * Quaternion.Inverse(transform.rotation);
+				gun.transform.localRotation = gunRot;
+			}
+			else
+			{
+				transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * rotSpeed);
+			}
+
+		}
+		animator.SetFloat("speed", Mathf.Abs(move.magnitude));
+		animator.SetBool("move", move.raw.magnitude != 0);
+
+
+		stunTime -= Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (grounded)
+			{
+				move.y = 1;
+			}
+		}
+
+	}
+	void LateUpdate()
+	{
+		Vector3 pos = transform.position + camOffset;
+		pos.z = Mathf.Lerp(cam.transform.position.z, pos.z, Time.deltaTime * camSpeed);
+		cam.transform.position = pos;
+	}
+	void FixedUpdate()
+	{
+		Vector3 v = rb.velocity;
+		grounded = Physics.Raycast(transform.position, -Vector3.up, 0.6f);
+		if (stunTime <= 0)
+		{
+			v.x = move.x * speed;
+			v.z = move.z * speed;
+		}
+		if (move.y > 0)
+		{
+			v.y += 3f;
+			move.y = 0;
+		}
+		rb.velocity = v;
+	}
+}
+
+public class Move
+{
+	public float x, y, z;
+	public Vector3 raw;
+	public Move()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		Debug.Log("Move created");
+	}
+	public void Update()
+	{
+		x = Input.GetAxis("Horizontal");
+		z = Input.GetAxis("Vertical");
+		raw.x = Input.GetAxisRaw("Horizontal");
+		raw.z = Input.GetAxisRaw("Vertical");
+	}
+	public float magnitude
+	{
+		get { return new Vector2(x, z).magnitude; }
+	}
+}
