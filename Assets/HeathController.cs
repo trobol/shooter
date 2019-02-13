8f7571d1bd3db6c30a46576317d9e9aa00f2fd48
Thornton <thorfernbacher@gmail.com>
2019-02-12 23:09:03 -0500

    adding hud

---
+++ Assets/HeathController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//font from: https://www.dafont.com/upheaval.font
+public class HeathController : MonoBehaviour {
+
+	public GameObject numE, anchor;
+
+	public GameObject dmgE;
+	public int currentValue;
+
+	void Start()
+	{
+		dmgE = Resources.Load<GameObject>("dmg");
+	}
+
+	public void SetHealth(int val) {
+
+	}
+	public void Dmg(int val) {
+
+	}
+	[ContextMenu("Dmg")]
+	public void Dmg() {
+		Plane plane = new Plane(Vector3.up, Vector3.up * 3);
+		Ray ray = Camera.main.ScreenPointToRay(anchor.transform.position);
+		float distance;
+		if (plane.Raycast(ray, out distance))
+		{
+			Vector3 hitPoint = ray.GetPoint(distance);
+			Instantiate(dmgE, hitPoint, Quaternion.identity);
+		}
+		
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+I got carried away making 3d models animated in 2d.
+
+The gameplay is identical to a 2d shooter but everything is 3d.
+You can only move on the x and z axes
